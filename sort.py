6490c71828c7b23e006c34fccd678e9d32aa7d45
Test <test@test.com>
2016-12-18 13:14:18 +0300

    Add sort.py

---
+++ sort.py
@@ -0,0 +1,30 @@
+'''
+Function sort
+
+'''
+
+def True():
+
+    return
+
+def False():
+
+    return
+
+
+def Func():
+    return
+
+
+InDim[22,34,23,12,64]
+
+OutDim[]
+
+for i in InDim :
+    if i = True():
+        OutDim() = i
+
+    if i = False():
+        OutDim() = Func(i)
+
+
